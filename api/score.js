aaf34529b4427f324f52eb6f49960e9646d99b01
CarlConnor <carl.connor@accaglobal.com>
2025-11-06 16:26:50 +0000

    Update score.js

--- api/score.js
+++ api/score.js
@@ -41,8 +41,8 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ feedback });
   } catch (err) {
-  console.error("Relay error:", err);
-  return res.status(500).json({ error: err.message || err });
+  console.error("Relay function error:", err);
+  res.status(500).json({ error: err.message || err.toString() });
 }
 }
 
